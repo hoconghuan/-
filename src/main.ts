8155e9f047163a32d35e69f7d97b8ae89d15714c
andydo365 <huconghuan@126.com>
2024-02-26 13:35:08 +0800

    change path

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import { createApp } from 'vue'
-import './reset.scss'
-import { router } from "@/routrer/index";
-import App from './App.vue'
-import Header from '@/Layout/Header/index.vue';
-import Footer from '@/Layout/Footer/index.vue';
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import { createApp } from "vue";
+import "./reset.scss";
+import { router } from "@/router/index";
+import App from "./App.vue";
+import Header from "@/Layout/Header/index.vue";
+import Footer from "@/Layout/Footer/index.vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 //@ts-ignore
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
 
-createApp(App).use(ElementPlus, {locale: zhCn,}).use(router).component('Header',Header).component('Footer',Footer).mount('#app')
+createApp(App)
+  .use(ElementPlus, { locale: zhCn })
+  .use(router)
+  .component("Header", Header)
+  .component("Footer", Footer)
+  .mount("#app");
